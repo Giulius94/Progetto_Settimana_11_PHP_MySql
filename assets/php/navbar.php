5ee5eb2aea522ad6b56b89a82550cb5379559282
Giulius94 <giuliuscb@protonmail.com>
2024-02-10 11:53:14 +0100

    fix img navbar

--- assets/php/navbar.php
+++ assets/php/navbar.php
@@ -5,7 +5,7 @@ session_start();
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">My Library</a>
+    <a class="navbar-brand" href="#"><img src="assets/img/images.png" class="rounded-circle w-25" alt="book"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
